cbed557defce914d58082dad6fff148fd3178f47
Tomasz Łucki <36811777+lucki90@users.noreply.github.com>
2023-04-22 13:44:42 +0200

    Update Sklep.java

--- src/samouczekJavaSklepInternetowy/Sklep.java
+++ src/samouczekJavaSklepInternetowy/Sklep.java
@@ -15,9 +15,19 @@ public class Sklep {
 
     public void wyswietlTowary() {
         for (Przedmiot przedmiot : towaryNaStanie) {
-            System.out.println(przedmiot);
+            System.out.println(przedmiot); //o wlasnie o takie wypisywanie przedmiotow na ekran mi chodzilo. Good. Ale dobrze bedzie zrobic to tak, zeby uzytkownik wiedzial jaki indeks ma dany przedmiot
+            //np zeby wyswietlily sie w takiej formie:
+            //1. lopata
+            //2. grabie
+            //3. ziemia
+            
+           // i wtedy jak uzytkownik bedzie chcial dodac grabie, to wpisze w konsoli '2' 
         }
     }
 
-
+    //metody na przyszlosc:
+    //dodajNowyProduktDoSklepu
+    //usunProduktZOferty
+    
+    //obsluga ilosci towarow na stanie. Np. sklep ma do dyspozycji 10 lopat, jak uzytkownik pobierze jedna do koszyka to zostanie 9 itd. a jak sie skoncza to wypisac komunikat
 }
