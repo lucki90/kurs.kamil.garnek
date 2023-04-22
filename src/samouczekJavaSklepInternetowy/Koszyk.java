971a2233c3e875ed8745cd84f535ffcb6d8f69b2
Tomasz Łucki <36811777+lucki90@users.noreply.github.com>
2023-04-22 13:37:17 +0200

    Update Koszyk.java

--- src/samouczekJavaSklepInternetowy/Koszyk.java
+++ src/samouczekJavaSklepInternetowy/Koszyk.java
@@ -4,17 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Koszyk {
+public class Koszyk { //rob ctrl + alt + l - zeby ladniej Ci sie kod formatowal, bo np. powinien byc odstep miedzy polami a metodami
 
-    private List<Double> towaryWKoszyku = new ArrayList<Double>();
+    private List<Double> towaryWKoszyku = new ArrayList<Double>(); //to powinna byc Lista<Towar>... 
     Scanner scanner = new Scanner(System.in);
-    public void dodajPrzedmiotDoKoszyka(){
-        System.out.println("Podaj numer z listy");
+    public void dodajPrzedmiotDoKoszyka(){ 
+        System.out.println("Podaj numer z listy"); //troche bez sensu. Na poczatek tutaj w parametrze powinna byc przekazana list<Towarow> w parametrze i z tej listy towarow uzytkownik powinien pobrac
+        //element listy o numerze ktory wpisze w konsoli i wtedy dodac go do listy towaryWKoszyku
         double numer = scanner.nextInt();
         towaryWKoszyku.add(numer);
     }
 
-    public List<Double> twojKoszyk(){
+    public List<Double> twojKoszyk(){ //lepiej wyswietlac elementy listy w petli
         return towaryWKoszyku;
     }
 
